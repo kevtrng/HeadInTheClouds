aa5ee6456b2041d8eb2a08b2b003505087f73576
caliLai <calilai2018@gmail.com>
2021-04-12 04:16:27 -0700

    file for generator

---
+++ random.php
@@ -0,0 +1,17 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+// require "./php/utility.php";
+require "./database/database.php";
+$pdo = db_connect();
+// handle_form_submission();
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+
+	</head>
+	<body>
+		<button>Random Cloud</button>
+	</body>
+</html>
