303c1b3083d3d3c6212a51cb3050b128ca0f54e9
caliLai <calilai2018@gmail.com>
2021-04-12 05:58:46 -0700

    im too lazy to move to another branch

--- random.php
+++ random.php
@@ -13,27 +13,29 @@ $pdo = db_connect();
 		<title>Head In The Clouds</title>
 
 		<link rel="stylesheet" href="./src/random.css">
+		<link rel="stylesheet" href="./src/cloud.css">
+		<link rel="stylesheet" href="./src/welcome.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	</head>
 	<body>
 
 		<nav class = "navBar">
-		<h3>HEAD IN THE CLOUDS</h3>
-		<!-- <img src="./src/images/logoCloud.png"> -->
-		<ul>
-			<li><a href="anotherPage.php">CLOUD WALL</a></li>
-			<li><a href="random.php">CLOUD GENERATOR</a></li>
-		</ul>
+			<h3>HEAD IN THE CLOUDS</h3>
+			<!-- <img src="./src/images/logoCloud.png"> -->
+			<ul>
+				<li><a href="anotherPage.php">CLOUD WALL</a></li>
+				<li><a href="random.php">CLOUD GENERATOR</a></li>
+			</ul>
 		</nav>
 
 
 		<h1>HEAD IN THE CLOUDS</h1>
 		<h2>RANDOM CLOUDS</h2>
 
+		<div id = "randCloud" class="cloud"><?php generateRand()?></div>
 
-		<form action="" method="" class = "button">
-			<input type="submit" value="Generate"/>
-		</form>
-		<?php generateRand()?>
+		<input type="button" value="Generate" id="randButton" class="enterButton"/>
+
 
 		<script src="./src/js/random.js"></script>
 	</body>
